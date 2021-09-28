84d8d46856107766d3def748a86c7c6e4345b673
myluke <luke75@163.com>
2021-09-28 22:39:05 +0800

    test

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"fmt"
-	"os"
 	"strconv"
 )
 
@@ -20,10 +19,31 @@ func main() {
 	i, _ := strconv.Atoi("-42")
 	s := strconv.Itoa(-42)
 	println(i, s)
-
-	number1 := os.Args[1]
-	number2 := os.Args[2]
+	// 计算
+	number1 := "2"
+	number2 := "3"
 	println("Sum:", sum(number1, number2))
+	// defer 关键字
+	name := "go"
+	defer printName(name) // output: go
+	name = "python"
+	defer printName(name) // output: python
+	name = "java"
+	printName(name) // output: java
+	deferTestA()
+}
+
+func deferTestA() {
+	i := 0
+
+	defer fmt.Println(i) //因为i=0，所以此时就明确告诉golang在程序退出时，执行输出0的操作
+	i++
+	defer fmt.Println(i) //输出1，因为i此时就是1
+	return
+}
+
+func printName(name string) {
+	fmt.Println(name)
 }
 
 func sum(a string, b string) int {
